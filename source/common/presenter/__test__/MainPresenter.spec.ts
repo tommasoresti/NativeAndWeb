cd1779099aa9b6a76770a9b816c4d5718f29288f
Tommaso Resti <tommaso.resti@lastminute.com>
2017-06-06 01:11:18 +0100

    Clean test

--- source/common/presenter/__test__/MainPresenter.spec.ts
+++ source/common/presenter/__test__/MainPresenter.spec.ts
@@ -1,18 +1,15 @@
 import {MainPresenter, MainView} from "../MainPresenter";
 import {IMock, Mock, Times, It} from 'typemoq'
 
-describe("a simple suite", () => {
+let presenter: MainPresenter;
+let view: IMock<MainView>
 
-  let presenter: MainPresenter;
-  let view: IMock<MainView>
+beforeEach(() => {
+  view = Mock.ofType<MainView>()
+  presenter = new MainPresenter(view.object)
+});
 
-  beforeEach(() => {
-    view = Mock.ofType<MainView>()
-    presenter = new MainPresenter(view.object)
-  });
-
-  test("on start should present the app name", () => {
-    presenter.onStart()
-    view.verify(x => x.showAppName(It.isAnyString()), Times.exactly(1))
-  })
+test("on start should present the app name", () => {
+  presenter.onStart()
+  view.verify(x => x.showAppName(It.isAnyString()), Times.atLeastOnce())
 })
